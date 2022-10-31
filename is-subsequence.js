1112f299f2e21ea9d2d6dc1450a5956b03f9c911
Andrea Bozzelli <74464364+bozzelliandrea@users.noreply.github.com>
2022-10-31 09:41:56 +0100

    Update is-subsequence.js

--- is-subsequence.js
+++ is-subsequence.js
@@ -30,3 +30,17 @@ var isSubsequence = function(s, t) {
     
     return chars.length == 0;
 };
+
+// without array
+var isSubsequence = function(s, t) {
+    
+    let j=0;
+    
+    for(let i = 0; i<t.length; i++) {
+      if(s[j] == t[i])
+         j++;
+        
+    }  
+    
+    return j == s.length;
+};
