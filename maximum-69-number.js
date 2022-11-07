450af9bfc2dcfc42910cf299627c980795d04ed4
Andrea Bozzelli <74464364+bozzelliandrea@users.noreply.github.com>
2022-11-07 14:22:17 +0100

    Create maximum-69-number.js

---
+++ maximum-69-number.js
@@ -0,0 +1,35 @@
+/**
+LeetCode: https://leetcode.com/problems/maximum-69-number/description/
+
+You are given a positive integer num consisting only of digits 6 and 9.
+
+Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+ 
+
+Example 1:
+
+Input: num = 9669
+Output: 9969
+Explanation: 
+Changing the first digit results in 6669.
+Changing the second digit results in 9969.
+Changing the third digit results in 9699.
+Changing the fourth digit results in 9666.
+The maximum number is 9969.
+
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+    const nums = (num + "").split("");
+
+    for(let i = 0; i<nums.length; i++) {
+       if(nums[i] == '6') {
+           nums[i] = '9';
+           break;
+       } 
+    }    
+
+    return parseInt(nums.join(""));
+};
